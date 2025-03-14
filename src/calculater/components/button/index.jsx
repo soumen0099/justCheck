551f959edf68e58ef1b1d03cd3db7fba49df1e03
soumen0099 <janasoumen14554@gmail.com>
2025-03-14 22:54:30 +0530

    fistfeatchur

--- src/calculater/components/button/index.jsx
+++ src/calculater/components/button/index.jsx
@@ -1,9 +1,9 @@
 import './style.css'
 function Button ({label='',onClick=()=>{},dataOperation}={}){
 
-  // function handleInput(e){
-  //   onInput(e.target.value)
-  // }
+  function handleInput(e){
+    onInput(e.target.value)
+  }
 
   return <button data-operation={dataOperation} onClick={onClick} >{label}</button>
 }
